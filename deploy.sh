d5a830e44c8fcf6cb1f52aa84f1e890696467cb3
mkw <68985422+mkw223@users.noreply.github.com>
2021-05-04 20:03:01 +0800

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd dist
+
+# 部署到自定义域名，没有自定义域名可注释掉
+# echo 'mkw223.github.io' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+git push -f git@github.com:mkw223/mkw2/.git master:gh-pages
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:mkw223/mkw223.github.io.git master
+
+cd -
+
